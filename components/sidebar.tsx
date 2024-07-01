37c9839da88d81556143222ae9212185987549bd
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-07-01 15:36:07 +0700

    add navbar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,14 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import {
+    ClerkLoading,
+    ClerkLoaded,
+    UserButton,
+} from "@clerk/nextjs"
+
+import { Loader } from "lucide-react";
 
 type Props = {
     className?: string;
@@ -6,10 +16,32 @@ type Props = {
 
 export const Sidebar = ({ className }: Props) => {
     return (
-        <div className={cn("flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        <div className={cn(
+            "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
             className,
         )}>
-            Sidebar
+            <Link href="/learn">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="/mascot.svg" alt="logo" width={40} height={40} />
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">Tansan</h1>
+                </div>
+            </Link>
+
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+                <SidebarItem label="Leaderboard" href="/leaderboard" iconSrc="/leaderboard.svg" />
+                <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
+                <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+            </div>
+
+            <div className="p-4">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton />
+                </ClerkLoaded>
+            </div>
         </div>
     );
 }
