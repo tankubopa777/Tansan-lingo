a99d7447075ffa6d9b94609ed6557002fc52e5da
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-07-01 14:04:07 +0700

    update marketing page

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,9 @@
+const LearnPage = () => {
+    return (
+        <div>
+            <h1>Learn</h1>
+        </div>
+    );
+}
+
+export default LearnPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import "./globals.css";
 
 const font = Nunito({ subsets: ["latin"] });
@@ -15,8 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
