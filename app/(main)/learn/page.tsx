c936ed2b91a5678325358e2f9b111ed4358f87cd
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-07-01 22:20:17 +0700

    update learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import { Header } from "./header"
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
     return (
-        <div>
-            <h1>Learn</h1>
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish" />
+            </FeedWrapper>
         </div>
     );
 }
