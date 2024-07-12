2114dbb25b6665e6001b61fb4ffeac4f497f4e22
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-07-12 14:56:16 +0700

    update fix button

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -44,6 +44,7 @@ const LearnPage = async () => {
                 ))}
             </FeedWrapper>
         </div>
+        
     );
 }
 
--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -32,19 +32,21 @@ export const Unit = ({
             <UnitBanner title={title} description={description} />
             <div className='flex items-center flex-col relative'>
                 {lessons.map((lesson, index) => {
-                    const isCurrent = lesson.id === activeLesson?.id;
+                    const isCurrent = true || lesson.id === activeLesson?.id;
                     const isLocked = !lesson.completed && !isCurrent;
 
                     return (
-                        <LessonButton
-                            key={lesson.id}
-                            id={lesson.id}
-                            index={index}
-                            totalCount={lessons.length - 1}
-                            current={isCurrent}
-                            locked={isLocked}
-                            percentage={activeLessonPercentage}
-                        />
+                        <div>
+                            <LessonButton
+                                key={lesson.id}
+                                id={lesson.id}
+                                index={index}
+                                totalCount={lessons.length - 1}
+                                current={isCurrent}
+                                locked={isLocked}
+                                percentage={activeLessonPercentage}
+                            />
+                        </div>
                     )
                 })}
             </div>
