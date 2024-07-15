6bd6a786debb643b78d3652f117da38df63f4c8b
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-07-15 13:13:39 +0700

    update queries

--- db/queries.ts
+++ db/queries.ts
@@ -3,7 +3,7 @@ import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 
 import db from "@/db/drizzle";
-import { challenges, courses, units, userProgress } from "@/db/schema";
+import { challengeProgress, challenges, courses, lessons, units, userProgress } from "@/db/schema";
 import { challengeProgressRelations } from './schema';
 
 export const getUserProgress = cache(async () => {
@@ -90,4 +90,91 @@ export const getUserProgressWithCourse = async () => {
     };
 };
 
-// Continue here
+export const getCourseProgress = cache(async () => {
+    const { userId } = await auth();
+    const userProgress = await getUserProgress();
+
+    if (!userId || !userProgress?.activeCourseId) {
+        return null;
+    }
+
+    const unitsInActiveCourse = await db.query.units.findMany({
+        orderBy: (units, { asc }) => [asc(units.order)],
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lessons: {
+                orderBy: (lessons, { asc }) => [asc(lessons.order)],
+                with: {
+                    unit: true,
+                    challenges: {
+                        with: {
+                            challengeProgress: {
+                                where: eq(challengeProgress.userId, userId),
+                            },
+                        }
+                    },
+                },
+            }
+        }
+    });
+
+    const firstUncompletedLesson = unitsInActiveCourse
+        .flatMap((unit) => unit.lessons)
+        .find((lesson) => {
+            return lesson.challenges.some((challenge) => {
+                return (
+                    !challenge.challengeProgress ||
+                    challenge.challengeProgress.length === 0 ||
+                    challenge.challengeProgress.some((progress) => !progress.completed)
+                );
+            });
+        });
+
+    return {
+        activeLesson: firstUncompletedLesson,
+        activeLessonId: firstUncompletedLesson?.id,
+    };
+});
+
+export const getLesson = cache(async (id?: number) => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const courseProgress = await getCourseProgress();
+
+    const lessonId = id || courseProgress?.activeLessonId;
+
+    if (!lessonId) {
+        return null;
+    }
+
+    const data = await db.query.lessons.findFirst({
+        where: eq(lessons.id, lessonId),
+        with: {
+            challenges: {
+                orderBy: (challenges, { asc }) => [asc(challenges.order)],
+                with: {
+                    challengeOptions: true,
+                    challengeProgress: {
+                        where: eq(challengeProgress.userId, userId),
+                    },
+                },
+            },
+        },
+    })
+
+    if (!data || !data.challenges) {
+        return null;
+    }
+
+    const normalizedChallenges = data.challenges.map((challenge) => {
+        const completed = challenge.challengeProgress && challenge.challengeProgress.length > 0 && challenge.challengeProgress.every((progress) => progress.completed);
+
+        return { ...challenge, completed };
+    });
+
+    return { ...data, challenges: normalizedChallenges };
+})
